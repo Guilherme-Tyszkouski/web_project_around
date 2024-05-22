a04233325976a766cf030804005e692ea86f57ab
Guilherme Tyszkouski <guilhermetyszkouski@gmail.com>
2024-05-22 19:02:59 -0300

    add button selection to javascript

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const DOM_editButtonProfile = document.querySelector(".profile__edit-button");
+const DOM_closeButtonProfile = document.querySelector(".edit__button-close");
+const DOM_editForm = document.querySelector(".edit");
+const DOM_formElement = document.querySelector(".form");
+
+
