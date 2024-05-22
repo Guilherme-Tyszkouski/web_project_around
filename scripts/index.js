17b5c303b8c3ae1970129c1ae930fad283c06f0c
Guilherme Tyszkouski <guilhermetyszkouski@gmail.com>
2024-05-22 19:14:58 -0300

    add pop up form function

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,18 @@ const DOM_closeButtonProfile = document.querySelector(".edit__button-close");
 const DOM_editForm = document.querySelector(".edit");
 const DOM_formElement = document.querySelector(".form");
 
+function handeleProfileFormSubmit(event) {
+  event.preventDefault();
+  const nameInput = document.querySelector(".form__input_name");
+  const jobInput = document.querySelector(".form__input_about");
+  const nameInputValue = nameInput.value.trim();
+  const jobInputInputValue = jobInputInput.value.trim();
+  const isEmptyNameInputValue = nameInputValue.length === 0;
+  const isEmptyJobInputValue = jobInputValue.length === 0;
 
+  if (isEmptyNameInputValue || isEmptyJobInputValue) {
+    alert('Preencha todos os campos!');
+    return;
+
+  }
+}
