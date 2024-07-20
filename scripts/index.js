acc113afcd8e5f4b53d208c4bdcfd2258e3afc12
Guilherme Tyszkouski <guilhermetyszkouski@gmail.com>
2024-07-20 00:29:36 -0300

    feat : add javascript function for initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,31 +7,39 @@ const DOM_editButtonPlaces = document.querySelector(".profile__add-image");
 const DOM_closeButtonPlaces = document.querySelector(".places__close");
 const DOM_placesForm = document.querySelector(".places");
 const DOM_placesFormElement = document.querySelector(".places__form");
+const DOM_elementsCards = document.querySelector(".elements__cards");
+const DOM_buttonAddImage = document.querySelector("[data-button=add-image]");
 
 const DOM_initialCards = [
   {
     name: "Vale de Yosemite",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    alt: "Rio passando no meio de arvores",
   },
   {
     name: "Lago Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    alt: "lago no meio de Montanhas",
   },
   {
     name: "Montanhas Carecas",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+    alt: "Por do sol em altas montanhas",
   },
   {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+    alt: "Céu estrelado com montanhas cobertas de neve",
   },
   {
     name: "Parque Nacional da Vanoise ",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+    alt: "Lago com montanhas com neve no topo",
   },
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+    alt: "Altas montanhas com um lago, com uma ponte e varios barcos de madeira",
   },
 ];
 
@@ -61,6 +69,85 @@ function handleProfileFormSubmit(event) {
   DOM_editForm.classList.remove("edit__visible");
 }
 
+//funcao que carrega as imagens inicias
+
+function createCards() {
+  DOM_initialCards.forEach((obj) => {
+    const li_tag = document.createElement("li");
+    li_tag.classList.add("elements__card");
+
+    const img_tag = document.createElement("img");
+    img_tag.setAttribute("src", obj.link);
+    img_tag.setAttribute("alt", obj.alt);
+    img_tag.classList.add("elements__card-image");
+
+    const div_tag = document.createElement("div");
+    div_tag.classList.add("elements__card-info");
+
+    const p_tag = document.createElement("p");
+    p_tag.classList.add("elements__card-name");
+    p_tag.textContent = obj.name;
+
+    const imgLike_tag = document.createElement("img");
+    imgLike_tag.setAttribute(
+      "src",
+      "../images/images-elements/elements-like.svg"
+    );
+    imgLike_tag.setAttribute("alt", obj.alt);
+    imgLike_tag.classList.add("elements__card-button");
+
+    li_tag.appendChild(img_tag);
+    li_tag.appendChild(div_tag);
+    div_tag.appendChild(p_tag);
+    div_tag.appendChild(imgLike_tag);
+
+    DOM_elementsCards.appendChild(li_tag);
+    console.log(DOM_elementsCards);
+  });
+}
+
+//funcao pra alterar imagem do site
+
+function createCard() {
+  event.preventDefault();
+  DOM_initialCards.forEach((obj) => {
+    const li_tag = document.createElement("li");
+    li_tag.classList.add("elements__card");
+
+    const img_tag = document.createElement("img");
+    img_tag.setAttribute("src", obj.link);
+    img_tag.setAttribute("alt", obj.alt);
+    img_tag.classList.add("elements__card-image");
+
+    const div_tag = document.createElement("div");
+    div_tag.classList.add("elements__card-info");
+
+    const p_tag = document.createElement("p");
+    p_tag.classList.add("elements__card-name");
+    p_tag.textContent = obj.name;
+
+    const imgLike_tag = document.createElement("img");
+    imgLike_tag.setAttribute(
+      "src",
+      "../images/images-elements/elements-like.svg"
+    );
+    imgLike_tag.setAttribute("alt", obj.alt);
+    imgLike_tag.classList.add("elements__card-button");
+
+    li_tag.appendChild(img_tag);
+    li_tag.appendChild(div_tag);
+    div_tag.appendChild(p_tag);
+    div_tag.appendChild(imgLike_tag);
+
+    DOM_elementsCards.appendChild(li_tag);
+    console.log(DOM_elementsCards);
+  });
+}
+
+//Botao adicionar imagem
+
+DOM_buttonAddImage.addEventListener("click", (event) => createCard(event));
+
 // Modal Section Profile
 
 DOM_formElement.addEventListener("submit", (event) =>
@@ -81,9 +168,8 @@ DOM_editButtonPlaces.addEventListener("click", () => {
   DOM_placesForm.classList.add("places__visible");
 });
 
-
 DOM_closeButtonPlaces.addEventListener("click", () => {
   DOM_placesForm.classList.remove("places__visible");
 });
 
-
+window.addEventListener("DOMContentLoaded", createCards);
